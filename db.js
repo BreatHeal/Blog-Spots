aef2bb0545560220c8df8a1ee7c5c29417bed481
BreathHeal <brethel.b.chavez@gmail.com>
2023-12-20 10:48:15 +0800

    changed the database configuration to clever cloud

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'blog-spots',
+    host: 'b6ky0e9rv7ncnqhi78ss-mysql.services.clever-cloud.com',
+    user: 'uapmxwq1li2hyehm',
+    password: 'GzbFqZpup9gmGE6IZ5Yv',
+    database: 'b6ky0e9rv7ncnqhi78ss',
 });
 
 module.exports = pool;
